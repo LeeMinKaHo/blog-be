e819b60800174ee8934525050f4de382b0e45a5f
leminhkhoa2002 <leminhkhoa2003@evolutee.net>
2026-04-03 19:12:13 +0700

    fix data source

--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -3,14 +3,17 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
-export const AppDataSource = {
-  //type: (process.env.DB_TYPE as any) || 'mysql',
-  host: process.env.DATABASE_HOST || 'localhost',
-  port: process.env.DATABASE_PORT ? parseInt(process.env.DATABASE_PORT) : 3306,
-  username: process.env.DATABASE_USER || 'root',
-  password: process.env.DATABASE_PASSWORD || 'LeKhoa@123',
-  database: process.env.DATABASE_NAME || 'blog',
-};
+export const AppDataSource: any = process.env.DATABASE_URL
+  ? { url: process.env.DATABASE_URL }
+  : {
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: process.env.DATABASE_PORT
+        ? parseInt(process.env.DATABASE_PORT)
+        : 3306,
+      username: process.env.DATABASE_USER || 'root',
+      password: process.env.DATABASE_PASSWORD || 'LeKhoa@123',
+      database: process.env.DATABASE_NAME || 'blog',
+    };
 
 export default new DataSource({
   ...AppDataSource,
